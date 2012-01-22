d3f4fd04fa743f36d61c7a15824d5abc68db6a0d
dmnlk <seikima2demon@gmail.com>
2012-01-23 01:51:32 +0900

    more smart?

--- rikunabi.js
+++ rikunabi.js
@@ -1,34 +1,19 @@
 // https://job.rikunabi.com/2013/auth/topLoginform/?toplocationUrl=&topPattern=top&sslMode=1&menu=T
 // https://sp.job.rikunabi.com/lnk000/=https://job.rikunabi.com/2013/auth/topLoginform/?topPattern=top&topLocationUrl=&sslMode=0&menu=T/
-
+function simulateClick(obj) {
+    var evt = document.createEvent("MouseEvents");
+    evt.initMouseEvent("click", true, true, window,
+                       0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    obj.dispatchEvent(evt);
+}
+var f = document.forms[0];
+f.rikunabiCd.value = "hoge";
+f.password.value = "hoge";
+f.auto.checked = true;
 var url ="https://job.rikunabi.com/2013/auth/topLoginform/?toplocationUrl=&topPattern=top&sslMode=1&menu=T";
 if(document.location==url){
-    function simulateClick(obj) {
-        var evt = document.createEvent("MouseEvents");
-        evt.initMouseEvent("click", true, true, window,
-                           0, 0, 0, 0, 0, false, false, false, false, 0, null);
-        obj.dispatchEvent(evt);
-    }
-    var f = document.forms[0];
-    f.rikunabiCd.value = "hoge";
-    f.password.value = "hoge";
-    f.auto.checked = true;
     var btn = document.getElementsByName("login")[0];
-    simulateClick(btn);
 }else{
-
-    var f = document.forms[0];
-
-    f.rikunabiCd.value = "hoge";
-    f.password.value = "hoge";
-    f.auto.checked = true;
     var btn = f.lastElementChild.firstChild;
-    simulateClick(btn);
-
-    function simulateClick(obj) {
-        var evt = document.createEvent("MouseEvents");
-        evt.initMouseEvent("click", true, true, window,
-                           0, 0, 0, 0, 0, false, false, false, false, 0, null);
-        obj.dispatchEvent(evt);
-    }
 }
+simulateClick(btn);
